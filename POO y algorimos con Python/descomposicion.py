023374a48f5ed8bca4a7dbbbc35b13658482da0e
Kevin Lopez <kevinjeff04@gmail.com>
2020-09-19 18:29:58 -0500

    Nunca pares de aprender

---
+++ POO y algorimos con Python/descomposicion.py
@@ -0,0 +1,22 @@
+class Automovil:
+    def __init__(self, modelo, marca, color):
+        self.modelo = modelo
+        self.marca = marca
+        self.color = color
+        self._estado = "en_reposo"
+        self._motor = Motor(cilindros = 4)
+
+    def acelerar(self, tipo = 'despacio'):
+        if tipo == 'rapida':
+            self._motor.inyectar_gasolina(10)
+        else:
+            self._motor.inyectar_gasolina(3)
+
+class Motor:
+    def __init__(self, cilindros, tipo = "gasolina"):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self._temperatura = 0
+
+    def inyectar_gasolina(self, cantidad):
+        pass

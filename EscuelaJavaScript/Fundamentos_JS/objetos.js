22f2cb0f8c359727385a97f8db8ebea0a36d2be1
Kevin Lopez <kevinjeff04@gmail.com>
2020-09-28 01:54:00 -0500

    Actualizando

--- EscuelaJavaScript/Fundamentos_JS/objetos.js
+++
@@ -1,5 +0,0 @@
-var persona = {
-    nombre: 'Kevin',
-    apellido: 'Lopez',
-    edad: 23
-}
--- EscuelaJavaScript/Fundamentos_JS/variablesNumeros.js
+++
@@ -1,5 +0,0 @@
-var edad = 23;
-edad +=1; //--> edad = edad+1
-var peso = 75; 
-//peso = peso-2;
-peso -= 2;
